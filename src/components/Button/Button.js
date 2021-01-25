ae479e12718f125ad8df7930436006db4137c76b
mateuszral <mateuszral@onet.pl>
2021-01-25 17:18:10 +0100

    Add button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.black};
+  color: ${({ theme }) => theme.white};
+  font-size: ${({ theme }) => theme.font.size.button};
+  font-weight: ${({ theme }) => theme.font.style.semiBold};
+  border: none;
+  padding: 10px 25px;
+  flex-shrink: 0;
+`;
+
+export default Button;
