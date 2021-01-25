b940ee86d9640c81dc4f1780193e2bdf835d1e04
mateuszral <mateuszral@onet.pl>
2021-01-25 13:29:23 +0100

    Add navigation for mobile

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+
+import Hamburger from 'components/Hamburger/Hamburger';
 
 import { routes } from 'routes';
 
 const NavigationWrapper = styled.nav`
   position: fixed;
+  width: 100%;
   top: 0;
   left: 0;
-  padding: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+
+  ${({ theme }) => theme.mq.bigTablet} {
+    padding: 10px 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const StyledLogo = styled.span`
@@ -20,41 +26,80 @@ const StyledLogo = styled.span`
 `;
 
 const NavigationList = styled.ul`
+  width: 100%;
+  height: 100vh;
   list-style: none;
+  transform: ${({ isMobileMenuOpen }) => (isMobileMenuOpen ? 'translateX(0%)' : 'translateX(100%)')};
+  background-color: ${({ theme }) => theme.black};
+  color: ${({ theme }) => theme.white};
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: transform 0.3s ease-in-out;
+
+  ${({ theme }) => theme.mq.bigTablet} {
+    transform: none;
+    background-color: ${({ theme }) => theme.white};
+    color: ${({ theme }) => theme.black};
+    height: initial;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
 `;
 
 const NavigationListItem = styled.li`
-  font-weight: ${({ theme }) => theme.font.styles.bold};
+  font-weight: ${({ theme }) => theme.font.styles.semiBold};
   font-size: ${({ theme }) => theme.font.size.button};
-  margin-left: 30px;
+  margin: 20px 0;
+
+  ${({ theme }) => theme.mq.bigTablet} {
+    margin-left: 40px;
+  }
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: ${({ theme }) => theme.black};
+  color: inherit;
 `;
 
 const Navigation = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsMobileMenuOpen((prevState) => !prevState);
+  };
+
   return (
     <NavigationWrapper>
-      <StyledLogo>
-        <StyledLink to={routes.home}>HATTA</StyledLink>
-      </StyledLogo>
-      <NavigationList>
+      <NavigationList isMobileMenuOpen={isMobileMenuOpen}>
+        <StyledLogo>
+          <StyledLink onClick={() => setIsMobileMenuOpen(false)} to={routes.home}>
+            HATTA
+          </StyledLink>
+        </StyledLogo>
         <NavigationListItem>
-          <StyledLink to={routes.about}>about</StyledLink>
+          <StyledLink onClick={() => setIsMobileMenuOpen(false)} to={routes.about}>
+            about
+          </StyledLink>
         </NavigationListItem>
         <NavigationListItem>
-          <StyledLink to={routes.articles}>articles</StyledLink>
+          <StyledLink onClick={() => setIsMobileMenuOpen(false)} to={routes.articles}>
+            articles
+          </StyledLink>
         </NavigationListItem>
         <NavigationListItem>
-          <StyledLink to={routes.gallery}>gallery</StyledLink>
+          <StyledLink onClick={() => setIsMobileMenuOpen(false)} to={routes.gallery}>
+            gallery
+          </StyledLink>
         </NavigationListItem>
         <NavigationListItem>
-          <StyledLink to={routes.contact}>contact</StyledLink>
+          <StyledLink onClick={() => setIsMobileMenuOpen(false)} to={routes.contact}>
+            contact
+          </StyledLink>
         </NavigationListItem>
       </NavigationList>
+      <Hamburger handleLinkClick={handleLinkClick} isMobileMenuOpen={isMobileMenuOpen} />
     </NavigationWrapper>
   );
 };
