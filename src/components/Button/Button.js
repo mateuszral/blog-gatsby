bea87a8f151e0e59633ebbe3f6a3227d43dde80b
mateuszral <mateuszral@onet.pl>
2021-01-25 19:36:13 +0100

    Repair button font weight

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ const Button = styled.button`
   background-color: ${({ theme }) => theme.black};
   color: ${({ theme }) => theme.white};
   font-size: ${({ theme }) => theme.font.size.button};
-  font-weight: ${({ theme }) => theme.font.style.semiBold};
+  font-weight: ${({ theme }) => theme.font.styles.semiBold};
   border: none;
   padding: 10px 25px;
   flex-shrink: 0;
