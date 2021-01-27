64f0d31fa74b6f56e309f5eebdfcb655d398caf4
mateuszral <mateuszral@onet.pl>
2021-01-27 16:34:26 +0100

    Add article dummy data

---
+++ src/assets/data/articles.js
@@ -0,0 +1,26 @@
+export const articles = [
+  {
+    title: 'Colorful ideas',
+    content:
+      'Interior Architecture includes architectural and technical aspects of an interior spac. Keeping in mind a the artistic features. The technical aspects include material, plumbing, electricals, ventilation and ergonomics amongst other things.',
+    image: 'colorful-ideas.jpg',
+  },
+  {
+    title: 'Where do you keep it',
+    content:
+      'Space management and aesthetic appeal of the interiors. Interior Architecture and Design involves conceptualization and realization of a structure’s interiors by smoothly integrating the architectural aspect with the design of the built space. Interior design on the other hand is more to do with space management and aesthetic appeal of the interiors once the structure has been realized.',
+    image: 'where-do-you-keep-it.jpg',
+  },
+  {
+    title: 'Tidy or messy?',
+    content:
+      'A renovation project would best describe the difference between the two fields explicitly. Hence even though both interior architects and interior designers enhance the purpose and quality of interior spaces the profile of former is vast and interior design is a subset of it.',
+    image: 'tidy-or-messy.jpg',
+  },
+  {
+    title: 'Colorful ideas',
+    content:
+      'However, the industry nowadays is witnessing a surge in demand for ‘Interior Architects’. So far the readers have been clear about architecture and interior design as related yet distinctive professions, but most of us are trying to understand the difference between interior architecture and interior design as separate disciplines.',
+    image: 'comfortable-bedroom.jpg',
+  },
+];
